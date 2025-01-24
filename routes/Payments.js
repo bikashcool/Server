5fd84f197f088896fface80f6ddaa9f27c05508c
BikashKumar <bikashcool481@gmail.com>
2025-01-24 23:55:02 +0530

    payment routes

---
+++ routes/Payments.js
@@ -0,0 +1,10 @@
+// import the required modules
+const express = require("express")
+const router = express.router()
+
+const {capturePayment, verifySignature} = require("../controllers/Payments")
+const {auth, isInstructor, isStudent, isAdmin} = require("../middlewares/auth");
+router.post("/capturePayment", auth, isStudent, capturePayment)
+router.post("/verfiySignature", verifySignature)
+
+module.exports = router
